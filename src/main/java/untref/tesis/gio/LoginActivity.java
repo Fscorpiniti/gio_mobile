0b62d5c76277c2cf29242b3850f5e1433031d287
Fernando Scorpiniti <fernando.scorpiniti@gmail.com>
2017-03-24 11:58:09 -0300

    Commit inicial. Contiene activity con login y primer configuracion general.

---
+++ src/main/java/untref/tesis/gio/LoginActivity.java
@@ -0,0 +1,14 @@
+package untref.tesis.gio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.login_activity);
+    }
+
+}
