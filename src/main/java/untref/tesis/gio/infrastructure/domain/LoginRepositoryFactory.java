cbd82dd4577704aa5f8349629a7a257c322bb263
Fernando Scorpiniti <fernando.scorpiniti@gmail.com>
2017-04-01 10:29:30 -0300

    Cambio a interface en la factory de data store

--- src/main/java/untref/tesis/gio/infrastructure/domain/LoginRepositoryFactory.java
+++ src/main/java/untref/tesis/gio/infrastructure/domain/LoginRepositoryFactory.java
@@ -3,6 +3,7 @@ package untref.tesis.gio.infrastructure.domain;
 import untref.tesis.gio.infrastructure.net.ApiService;
 import untref.tesis.gio.infrastructure.net.LoginApiService;
 import untref.tesis.gio.infrastructure.repository.DefaultLoginRepository;
+import untref.tesis.gio.infrastructure.repository.datasource.LoginDataStore;
 import untref.tesis.gio.infrastructure.repository.datasource.ServerLoginDataStore;
 
 public class LoginRepositoryFactory {
@@ -11,7 +12,7 @@ public class LoginRepositoryFactory {
         return new DefaultLoginRepository(buildLoginDataStore());
     }
 
-    private static ServerLoginDataStore buildLoginDataStore() {
+    private static LoginDataStore buildLoginDataStore() {
         return new ServerLoginDataStore(ApiService.retrofit.create(LoginApiService.class));
     }
 }
