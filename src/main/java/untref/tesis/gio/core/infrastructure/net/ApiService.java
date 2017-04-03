d863bbf4223f3aab6a5a82e96e29712145dd84cd
Fernando Scorpiniti <fernando.scorpiniti@gmail.com>
2017-04-02 21:25:12 -0300

    cambio para llegar al server local

--- src/main/java/untref/tesis/gio/core/infrastructure/net/ApiService.java
+++ src/main/java/untref/tesis/gio/core/infrastructure/net/ApiService.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public interface ApiService {
 
     static final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://localhost:8080/")
+            .baseUrl("http://10.0.2.2:8080/")
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build();
