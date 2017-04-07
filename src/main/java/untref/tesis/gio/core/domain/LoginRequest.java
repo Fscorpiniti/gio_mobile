a2d369612b374df22efa9db292543d6326303fb4
Fernando Scorpiniti <fernando.scorpiniti@gmail.com>
2017-04-07 19:52:10 -0300

    Agrego tests del request

--- src/main/java/untref/tesis/gio/core/domain/LoginRequest.java
+++ src/main/java/untref/tesis/gio/core/domain/LoginRequest.java
@@ -1,6 +1,6 @@
 package untref.tesis.gio.core.domain;
 
-import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang3.StringUtils;
 
 public class LoginRequest {
 
@@ -9,8 +9,17 @@ public class LoginRequest {
     private String password;
 
     public LoginRequest(String email, String password) {
+        checkIsNotBlank(email, "Email is required");
+        checkIsNotBlank(password, "Password is required");
+
         this.email = email;
         this.password = password;
+    }
+
+    private void checkIsNotBlank(String value, String message) {
+        if (StringUtils.isBlank(value)) {
+            throw new IllegalArgumentException(message);
+        }
     }
 
     public String getEmail() {
