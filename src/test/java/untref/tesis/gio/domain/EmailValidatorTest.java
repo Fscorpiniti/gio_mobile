725da4523e1b40a7916c08dc5020040ad50ffa00
Fernando Scorpiniti <fernando.scorpiniti@gmail.com>
2017-05-06 19:38:46 -0300

    Cambio de excepcion por ValidationException

--- src/test/java/untref/tesis/gio/domain/EmailValidatorTest.java
+++ src/test/java/untref/tesis/gio/domain/EmailValidatorTest.java
@@ -3,38 +3,40 @@ package untref.tesis.gio.domain;
 
 import org.junit.Test;
 
+import untref.tesis.gio.presentation.exception.ValidationException;
+
 public class EmailValidatorTest {
 
     @Test
-    public void whenValidateCorrectEmailThenExceptionIsNotThrown() {
+    public void whenValidateCorrectEmailThenExceptionIsNotThrown() throws ValidationException {
         String correctEmail = "test@test.com";
 
         new EmailValidator().validate(correctEmail);
     }
 
-    @Test(expected = EmailInvalidException.class)
-    public void whenValidateEmailWitoutAtThenExceptionIsThrown() {
+    @Test(expected = ValidationException.class)
+    public void whenValidateEmailWitoutAtThenExceptionIsThrown() throws ValidationException {
         String emailWithoutAt = "testtest.com";
 
         new EmailValidator().validate(emailWithoutAt);
     }
 
-    @Test(expected = EmailInvalidException.class)
-    public void whenValidateEmailWitoutPointThenExceptionIsThrown() {
+    @Test(expected = ValidationException.class)
+    public void whenValidateEmailWitoutPointThenExceptionIsThrown() throws ValidationException {
         String emailWithoutPoint = "test@testcom";
 
         new EmailValidator().validate(emailWithoutPoint);
     }
 
-    @Test(expected = EmailInvalidException.class)
-    public void whenValidateEmailWitoutExtensionThenExceptionIsThrown() {
+    @Test(expected = ValidationException.class)
+    public void whenValidateEmailWitoutExtensionThenExceptionIsThrown() throws ValidationException {
         String emailWithoutExtension = "test@test.";
 
         new EmailValidator().validate(emailWithoutExtension);
     }
 
-    @Test(expected = EmailInvalidException.class)
-    public void whenValidateEmptyEmailThenExceptionIsThrown() {
+    @Test(expected = ValidationException.class)
+    public void whenValidateEmptyEmailThenExceptionIsThrown() throws ValidationException {
         String emptyEmail = "";
 
         new EmailValidator().validate(emptyEmail);
