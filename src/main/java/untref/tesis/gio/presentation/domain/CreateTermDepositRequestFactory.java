63f5d78d900ea734dc6549bc05b07bbef414e5a5
Fernando Scorpiniti <fernando.scorpiniti@gmail.com>
2017-05-16 22:41:49 -0300

    Cambio validacion para que soporte valores decimales

--- src/main/java/untref/tesis/gio/presentation/domain/CreateTermDepositRequestFactory.java
+++ src/main/java/untref/tesis/gio/presentation/domain/CreateTermDepositRequestFactory.java
@@ -5,7 +5,7 @@ import untref.tesis.gio.presentation.exception.ValidationException;
 
 public class CreateTermDepositRequestFactory {
 
-    public static final int MIN_VALUE = 0;
+    public static final Double MIN_VALUE = new Double(0);
 
     public CreateTermDepositRequest build(Integer ownerId, Double amount, Integer duration, Double rate)
             throws ValidationException {
@@ -15,21 +15,16 @@ public class CreateTermDepositRequestFactory {
     }
 
     private void validate(Integer ownerId, Double amount, Integer duration, Double rate) throws ValidationException {
-        validate(ownerId, "Owner id es invalido");
+        validate(ownerId.doubleValue(), "Owner id es invalido");
         validate(amount, "Cantidad es invalido");
-        validate(duration, "Duracion es invalida");
+        validate(duration.doubleValue(), "Duracion es invalida");
         validate(rate, "Porcenaje es invalido");
     }
 
     private void validate(Double value, String message) throws ValidationException {
-        if (value == null || value.intValue() <= MIN_VALUE) {
-            throw new ValidationException(message);
-        }
-    }
-
-    private void validate(Integer value, String message) throws ValidationException {
         if (value == null || value <= MIN_VALUE) {
             throw new ValidationException(message);
         }
     }
+
 }
