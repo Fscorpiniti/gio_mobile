1f24fd0c2cca5eeb4a91ae7cd00f914d63340b2c
Fernando Scorpiniti <fernando.scorpiniti@gmail.com>
2017-05-16 21:47:28 -0300

    Agrego un nuevo theme y recycer view

--- src/main/java/untref/tesis/gio/presentation/activity/DefaultDashboardActivity.java
+++ src/main/java/untref/tesis/gio/presentation/activity/DefaultDashboardActivity.java
@@ -1,25 +1,25 @@
 package untref.tesis.gio.presentation.activity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import java.util.List;
-import java.util.function.Function;
 
 import untref.tesis.gio.R;
 import untref.tesis.gio.domain.entity.TermDeposit;
+import untref.tesis.gio.presentation.adapter.ListTermDepositsAdapter;
 import untref.tesis.gio.presentation.domain.DashboardPresenterFactory;
 import untref.tesis.gio.presentation.presenter.DashboardPresenter;
 
-public class DefaultDashboardActivity extends AppCompatActivity implements DashboardActivity {
+public class DefaultDashboardActivity extends Activity implements DashboardActivity {
 
     private static final Integer DEFAULT_USER_ID = 0;
     private static final String DEFAULT_TOKEN = "0";
@@ -29,16 +29,9 @@ public class DefaultDashboardActivity extends AppCompatActivity implements Dashb
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.dashboard_activity);
+        createListOfTermDeposits();
         dashboardPresenter = DashboardPresenterFactory.build(this);
         findTermDepositsByOwner();
-    }
-
-    private void findTermDepositsByOwner() {
-        SharedPreferences sharedPref = getSharedPreferences("user", Context.MODE_PRIVATE);
-        Integer ownerId = sharedPref.getInt("user_id", DEFAULT_USER_ID);
-        String authToken = sharedPref.getString("auth_token", DEFAULT_TOKEN);
-
-        dashboardPresenter.findByOwner(ownerId, authToken);
     }
 
     @Override
@@ -67,24 +60,32 @@ public class DefaultDashboardActivity extends AppCompatActivity implements Dashb
 
     @Override
     public void completeTermDepositList(List<TermDeposit> termDeposits) {
-        ListView listView = (ListView) findViewById(R.id.list_term_deposits);
-        String[] values = buildList(termDeposits);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
-        listView.setAdapter(adapter);
-    }
-
-    private String[] buildList(List<TermDeposit> termDeposits) {
-       return termDeposits.stream().map(buildMessageList()).toArray(String[]::new);
-    }
-
-    private Function<TermDeposit, String> buildMessageList() {
-        return termDeposit -> new StringBuilder("Plazo fijo activo por ").append(termDeposit.getAmount())
-                .append(" con vencimiento en ").append(termDeposit.getExpiration()).toString();
+        RecyclerView recyclerView = findRecyclerView();
+        RecyclerView.Adapter adapter = new ListTermDepositsAdapter(termDeposits);
+        recyclerView.setAdapter(adapter);
     }
 
     private boolean isAction(MenuItem item, int actionId) {
         return item.getItemId() == actionId;
+    }
+
+    private RecyclerView findRecyclerView() {
+        return (RecyclerView) findViewById(R.id.list_term_deposits);
+    }
+
+    private void findTermDepositsByOwner() {
+        SharedPreferences sharedPref = getSharedPreferences("user", Context.MODE_PRIVATE);
+        Integer ownerId = sharedPref.getInt("user_id", DEFAULT_USER_ID);
+        String authToken = sharedPref.getString("auth_token", DEFAULT_TOKEN);
+
+        dashboardPresenter.findByOwner(ownerId, authToken);
+    }
+
+    private void createListOfTermDeposits() {
+        RecyclerView recyclerView = findRecyclerView();
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
     }
 
 }
---
+++ src/main/java/untref/tesis/gio/presentation/adapter/ListTermDepositsAdapter.java
@@ -0,0 +1,64 @@
+package untref.tesis.gio.presentation.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import untref.tesis.gio.R;
+import untref.tesis.gio.domain.entity.TermDeposit;
+
+public class ListTermDepositsAdapter extends RecyclerView.Adapter<ListTermDepositsAdapter.ViewHolder> {
+
+    private static final String PLAZO_FIJO_ACTIVO_POR = "Plazo fijo activo por: ";
+    private static final String FECHA_DE_VENCIMIENTO = "Fecha de vencimiento: ";
+    private static final String PESOS = " pesos.";
+    private static final String PATTERN = "dd/M/yyyy";
+    private List<TermDeposit> termDeposits;
+
+    public ListTermDepositsAdapter(List<TermDeposit> termDeposits) {
+        this.termDeposits = termDeposits;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list_term_deposit, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String title = new StringBuilder(PLAZO_FIJO_ACTIVO_POR).append(termDeposits.get(position)
+                .getAmount().toString()).append(PESOS).toString();
+        holder.amount.setText(title);
+        String expirationText = new StringBuilder(FECHA_DE_VENCIMIENTO)
+                .append(formatExpirationDate(position)).toString();
+        holder.expiration.setText(expirationText);
+    }
+
+    @Override
+    public int getItemCount() {
+        return termDeposits.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView amount;
+        public TextView expiration;
+
+        public ViewHolder(View view) {
+            super(view);
+            amount = (TextView) view.findViewById(R.id.title_term_deposit_list);
+            expiration =  (TextView) view.findViewById(R.id.expiration_term_deposit_list);
+        }
+    }
+
+    private String formatExpirationDate(int position) {
+        return new SimpleDateFormat(PATTERN).format(termDeposits.get(position).getExpiration());
+    }
+}
