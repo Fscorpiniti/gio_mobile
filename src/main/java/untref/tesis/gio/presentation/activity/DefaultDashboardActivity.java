93d67a52d1d7edafa85959aaf7af58a47b9b861d
Fernando Scorpiniti <fernando.scorpiniti@gmail.com>
2017-05-20 11:05:06 -0300

    Remuevo imports que no se usan

--- src/main/java/untref/tesis/gio/presentation/activity/DefaultDashboardActivity.java
+++ src/main/java/untref/tesis/gio/presentation/activity/DefaultDashboardActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
